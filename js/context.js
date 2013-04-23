a42e24b8bb5a9afecb237a6d2764627c426152a3
Zbigniew Braniecki <gandalf@mozilla.com>
2013-04-23 11:55:43 +0200

    add context.js

---
+++ js/context.js
@@ -0,0 +1,128 @@
+function Context(id) {
+  this.id = id;
+  this.data = {};
+
+  this.bindResource = bindResource;
+
+  this.get = get;
+  this.getEntity = getEntity;
+  this.localize = localize;
+
+  var _emitter = new L20n.EventEmitter();
+  var _parser = new L20n.Parser(L20n.EventEmitter);
+  var _compiler = new L20n.Compiler(L20n.EventEmitter, L20n.Parser);
+  var _globalsManager = new L20n.GlobalsManager();
+
+  var _ast = null;
+  var _source = null;
+  var _entries = null;
+
+  _compiler.setGlobals(_globalsManager.globals);
+
+  function bindResource(source) {
+    _source = source;
+    build();
+  }
+
+  function build() {
+    _ast = _parser.parse(_source);
+    _entries = _compiler.compile(_ast); 
+  }
+
+  function get(id, data) {
+    var entry = _entries[id];
+    if (entry === undefined) {
+      _emitter.emit('error', new EntityError("Not found", id, null));
+      return id;
+    } 
+    try {
+      return entry.getString(getArgs.bind(this, data));
+    } catch(e) {
+      if (e instanceof L20n.Compiler.RuntimeError) {
+        _emitter.emit('error', new EntityError(e.message, id, null));
+        return e.source;
+      } else {
+        throw e;
+      }
+    }
+    return entity.value; 
+  }
+
+  function getEntity(id, data) {
+    var entry = _entries[id];
+    if (entry === undefined) {
+      _emitter.emit('error', new EntityError("Not found", id, null));
+      return id;
+    }
+    return entry.get(getArgs.bind(this, data));
+  }
+
+  function localize(idsOrTuples, callback) {
+    var vals = {};
+    for (var i = 0, iot; iot = idsOrTuples[i]; i++) {
+      if (Array.isArray(iot)) {
+        vals[iot[0]] = getEntity(iot[0], iot[1]);
+      } else {
+        vals[iot] = getEntity(iot);
+      }
+    }
+    var globalsUsed = {};
+    Object.keys(vals).forEach(function (key) {
+      Object.keys(vals[key].globals).forEach(function (key2) {
+        globalsUsed[key2] = true;
+      });
+    });
+    var retobj = {
+      'entities': vals,
+      'globals': Object.keys(globalsUsed),
+    };
+    if (callback) {
+      callback(retobj);
+      _globalsManager.registerGet({
+        'id': callback,
+        'callback': localize.bind(this, idsOrTuples, callback),
+        'globals': retobj.globals});
+    }
+    return retobj;
+  }
+
+  function getArgs(data) {
+    if (!data) {
+      return this.data;
+    }
+    var args = {};
+    for (var i in this.data) {
+      if (this.data.hasOwnProperty(i)) {
+        args[i] = this.data[i];
+      }
+    }
+    if (data) {
+      for (i in data) {
+        if (data.hasOwnProperty(i)) {
+          args[i] = data[i];
+        }
+      }
+    }
+    return args;
+  }
+}
+
+Context.Error = ContextError;
+Context.EntityError = EntityError;
+
+function ContextError(message) {
+  this.name = 'ContextError';
+  this.message = message;
+}
+ContextError.prototype = Object.create(Error.prototype);
+ContextError.prototype.constructor = ContextError;
+
+function EntityError(message, id, lang) {
+  ContextError.call(this, message);
+  this.name = 'EntityError';
+  this.id = id;
+  this.lang = lang;
+  this.message = '[' + lang + '] ' + id + ': ' + message;
+}
+EntityError.prototype = Object.create(ContextError.prototype);
+EntityError.prototype.constructor = EntityError;
